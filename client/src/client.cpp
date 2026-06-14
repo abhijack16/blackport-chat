5b9b3d52519f6a5c992c0d67c7790e4bef1924dc
Sagar Singh Rautela <justsagarthing@gmail.com>
2026-06-14 20:11:39 +0530

    Add Winsock initialization

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,6 +1,19 @@
 #include "../include/client.h"
 #include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 void startClient() {
-    std::cout << "[BLACKPORT CLIENT STARTED]\n";
+
+    WSADATA wsData;
+
+    int result = WSAStartup(MAKEWORD(2, 2), &wsData);
+
+    if (result != 0) {
+        std::cout << "WSAStartup failed\n";
+        return;
+    }
+
+    std::cout << "[CLIENT] Winsock initialized\n";
 }
